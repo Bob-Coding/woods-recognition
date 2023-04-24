d1440a76b2d16221f25324364fdb3a055eaa982b
Bob-Coding <55443455+Bob-Coding@users.noreply.github.com>
2023-04-24 19:09:11 +0200

    added cropping/adding padding if needed to img

--- dashboard/app/main.py
+++ dashboard/app/main.py
@@ -4,8 +4,7 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from config import settings
 import csv
-from PIL import Image
-
+from PIL import Image, ImageOps
 
 from database.connection import MongoDBConnection
 from pymongo.errors import ConnectionFailure
@@ -43,13 +42,44 @@ try:
         if filename.endswith('.jpg'):
             # Open de afbeelding
             with Image.open('../data/images/' + filename) as img:
-                # Pas de grootte van de afbeelding aan
-                img_resized = img.resize(size)
-                # Sla de aangepaste afbeelding op met een andere bestandsnaam
+
+                # bepaal de gewenste afmetingen
+                target_width = 40
+                target_height = 40
+
+                # bereken de middelpunten
+                width, height = img.size
+                left = (width - target_width) / 2
+                top = (height - target_height) / 2
+                right = (width + target_width) / 2
+                bottom = (height + target_height) / 2
+
+                # bijsnijden en opslaan
                 img_path = '../data/images/resized/'
                 if not os.path.exists(img_path):
                     os.mkdir(img_path)
-                img_resized.save(img_path + filename)
+
+                # controleer of de afbeelding kleiner is dan de gewenste afmetingen
+                if width < target_width or height < target_height:
+                    # bereken de padding om toe te voegen
+                    horizontal_padding = max(target_width - img.width, 0) // 2
+                    vertical_padding = max(target_height - img.height, 0) // 2
+
+                    # voeg de padding toe en sla op
+                    img_padded = ImageOps.expand(img, border=(horizontal_padding, vertical_padding), fill="white")
+                    img_padded.save(img_path + filename)
+                else:
+                    # bijsnijden en opslaan
+                    img_cropped = img.crop((left, top, right, bottom))
+                    img_cropped.save(img_path + filename)
+
+
+                # # Pas de grootte van de afbeelding aan
+                # img_resized = img.resize(size)
+                # # Sla de aangepaste afbeelding op met een andere bestandsnaam
+                # if not os.path.exists(img_path):
+                #     os.mkdir(img_path)
+                # img_resized.save(img_path + filename)
     print("Pictures resized to " + str(size))
 
 except ConnectionFailure as e:
