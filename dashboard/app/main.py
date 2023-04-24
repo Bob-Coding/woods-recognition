57fb60599dec8f8a9932c8493acf60cc58625ab4
omarzakri <omar.zakri@hva.nl>
2023-04-24 12:39:35 +0200

    Add images, csv and strore in mongo

--- dashboard/app/main.py
+++ dashboard/app/main.py
@@ -1,3 +1,10 @@
+import os
+import sys
+import csv
+
+# Add the parent directory of the current file to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from config import settings
 from database.connection import MongoDBConnection
 from pymongo.errors import ConnectionFailure
@@ -11,9 +18,21 @@ try:
     print("Successfully connected to MongoDB!")
     print(f"Database URL: {settings.DATABASE_URL}")
     print(f"Database name: {settings.DATABASE_NAME}")
+
+    # Read data from CSV file
+    with open('../data/data.csv', 'r') as file:
+        reader = csv.DictReader(file)
+        data = [row for row in reader]
+
+    # Insert data into MongoDB
+    db.woods.insert_many(data)
+    print("Data successfully inserted into MongoDB!")
+
 except ConnectionFailure as e:
     print("Failed to connect to MongoDB.")
     print(e)
+
+# Finally close connection
 finally:
     if connection:
         connection.close()
