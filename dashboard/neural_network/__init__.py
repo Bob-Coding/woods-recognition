4a962d117e53e67a2bd0e24a081b5ed3c4375b48
Bob-Coding <55443455+Bob-Coding@users.noreply.github.com>
2023-05-23 19:51:54 +0200

    cleanup

--- dashboard/neural_network/__init__.py
+++
@@ -1,3 +0,0 @@
-# This file is required to mark the config directory as a Python package.
-# It may be empty, or it may contain configuration settings or other code that should be used throughout the app.
-# In this case, the app package imports the neural network module from the app package.
