455ba75647dc569d061866aeefbf5fa59b8b381b
Bob Gunsing <bob.gunsing@xillio.com>
2023-06-15 15:57:40 +0200

    fix small issues

--- service/config/settings.py
+++ service/config/settings.py
@@ -2,11 +2,11 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 API_URL = os.getenv("API_URL") or "http://localhost:5000"
-USERNAME= os.getenv("USERNAME") or "root"
-PASSWORD= os.getenv("PASSWORD") or "example"
-MONGO_URI= os.getenv("MONGO_URI") or f"mongodb://{USERNAME}:{PASSWORD}@localhost:27017/?authSource=admin" or "mongodb://root:example@localhost:27017/?authSource=admin"
+MONGO_USERNAME= os.getenv("MONGO_USERNAME") or "root"
+MONGO_PASSWORD= os.getenv("MONGO_PASSWORD") or "example"
+DATABASE_URL= os.getenv("MONGO_URI") or f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@localhost:27017/?authSource=admin" or "mongodb://root:example@localhost:27017/?authSource=admin"
 DATABASE_NAME = os.getenv("DATABASE_NAME") or "woods_recognition"
-DATABASE_URL = os.getenv("DATABASE_URL") or f"mongodb://{USERNAME}:{PASSWORD}@localhost:27017/{DATABASE_NAME}?authSource=admin"
+DATABASE_URL = os.getenv("DATABASE_URL") or f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@localhost:27017/{DATABASE_NAME}?authSource=admin"
 COLLECTION_NAME = os.getenv("COLLECTION_NAME") or "dataset_cleaned"
 TRAIN_DENSE_MODEL = os.getenv("TRAIN_DENSE_MODEL") or False
 TRAIN_SEQ_DENSE_MODEL = os.getenv("TRAIN_SEQ_DENSE_MODEL") or False
--- service/database/connection.py
+++ service/database/connection.py
@@ -7,6 +7,7 @@ from pymongo import MongoClient
 class MongoDBConnection:
     def __init__(self, url):
         self.url = url
+        print(self.url)
         self.client = MongoClient(self.url)
 
     def get_collection(self, collection_name):
@@ -15,7 +16,6 @@ class MongoDBConnection:
     
         db = self.client[settings.DATABASE_NAME]
         collection_names = db.list_collection_names()
-    
         if collection_name not in collection_names:
             # Collection does not exist, create it
             db.create_collection(collection_name)
@@ -29,14 +29,8 @@ class MongoDBConnection:
     def get_database(self):
         if self.client is None:
             self.client = MongoClient(self.url)
-        # check if the database exists
-        database_names = self.client.list_database_names()
-        if [settings.DATABASE_NAME] not in database_names:
-            # Create the database
-            db = self.client[settings.DATABASE_NAME]
-        else:
-            # Database already exists
-            db = self.client[settings.DATABASE_NAME]
+        
+        db = self.client[settings.DATABASE_NAME]
         return db
 
         
